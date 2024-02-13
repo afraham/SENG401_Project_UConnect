f083bab8f668ec4ad3726a945b005c945b5780a0
Ethan-Nguy <ethan.nguy@ucalgary.ca>
2024-02-12 18:53:41 -0700

    Navigation and Placeholder js files

---
+++ uconnect/src/FindEvents.js
@@ -0,0 +1,7 @@
+function FindEvents() {
+  return (
+    <h1>FindEvents</h1>
+  );
+}
+
+export default FindEvents;
---
+++ uconnect/src/MyEvents.js
@@ -0,0 +1,7 @@
+function MyEvents() {
+  return (
+    <h1>MyEvents</h1>
+  );
+}
+
+export default MyEvents;
