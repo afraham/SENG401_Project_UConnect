b31c912608896fc43e785f73ece3f103f167772d
afrah <afrahamerriarh@hotmail.com>
2024-03-17 21:00:49 -0600

    adjusted file structure for better organization

---
+++ uconnect/server/src/controllers/eventsController.js
@@ -0,0 +1,40 @@
+const db = require("../db/db");
+
+exports.createEvent = async (req, res) => {
+	try {
+		await db.connect();
+		const database = db.db("create_events");
+		const collection = database.collection("events");
+
+		const eventData = req.body;
+
+		const result = await collection.insertOne(eventData);
+
+		console.log(`Successfully inserted event with _id: ${result.insertedId}`);
+		res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+		res.status(201).send({ message: "Event created successfully" });
+	} catch (error) {
+		console.error("Error inserting event:", error);
+		res.status(500).send({ message: "Internal Server Error" });
+	} finally {
+		await db.close();
+	}
+};
+
+exports.getEvents = async (req, res) => {
+	try {
+		await db.connect();
+		const database = db.db("create_events");
+		const collection = database.collection("events");
+
+		const events = await collection.find().toArray();
+
+		res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+		res.status(200).json(events);
+	} catch (error) {
+		console.error("Error retrieving events:", error);
+		res.status(500).send({ message: "Internal Server Error" });
+	} finally {
+		await db.close();
+	}
+};
