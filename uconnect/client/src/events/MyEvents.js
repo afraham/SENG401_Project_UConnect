81525ba3cf8eb8102571c64969bc85fde3f1f611
Sudarshan Naicker <sudarshansudu1@gmail.com>
2024-03-19 13:01:46 -0600

    Working Myevents

--- uconnect/client/src/events/MyEvents.js
+++ uconnect/client/src/events/MyEvents.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./MyEvents.css";
+import "./FindEvents.css";
 import AddEvents from "./AddEvents";
 import { auth } from "../firebase";
 
@@ -48,18 +49,64 @@ function MyEvents() {
 		fetchEvents();
 	}, []);
 
+
+	const toggleExpansion = (index) => {
+        setEvents(currentEvents =>
+            currentEvents.map((event, i) => {
+                if (i === index) {
+                    return { ...event, isExpanded: !event.isExpanded };
+                }
+                return event;
+            })
+        );
+    };
+
 	return (
-		<div className="my-events-page">
-			<p>
-				<b>ADD AN EVENT!</b>
-			</p>
-			<div className="event-button-container">
-				<button className="event-button" onClick={handleShowPopup}>
-					{" "}
-					+{" "}
-				</button>
+		<div>
+			<div className="my-events-page">
+				<p><b>ADD AN EVENT!</b></p>
+				<div className="event-button-container">
+					<button className="event-button" onClick={handleShowPopup}>
+						{" "}
+						+{" "}
+					</button>
+				</div>
+				{showPopup && <AddEvents closePopup={handleClosePopup} />}
+				
 			</div>
-			{showPopup && <AddEvents closePopup={handleClosePopup} />}
+			<hr className="myevents-line"></hr>
+			<div className="event-list">
+				{Array.isArray(events) &&
+					events.map((event, index) => (
+						<div
+                            className={`event-card ${event.isExpanded ? 'expanded' : ''}`}
+                            key={index}
+                            onClick={() => toggleExpansion(index)}
+                        >
+							<div className="top-box">
+								<div className="left-align">
+									<p className="event-title">{event.title}</p>
+								</div>
+								<div className="right-align">
+									<p className="capacity">
+										{event.spotsTaken}/{event.maxPeople}
+										
+									</p>
+									<p className="capacity"><i class="fa fa-group"></i></p>
+								</div>
+							</div>
+							<p className={`description ${event.isExpanded ? 'expanded' : ''}`}>{event.description}</p>
+							<div className="bottom-box">
+								<div className="left-align">
+									<p className="location"><i class="fa fa-map-marker"></i> {event.location}</p>
+								</div>
+								<div className="right-align">
+									<button className="request-button">Edit</button>
+								</div>
+							</div>
+						</div>
+					))}
+			</div>
 		</div>
 	);
 }
