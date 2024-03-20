c906a3960340798d4c482c8dfaed14053c954a26
Maham Jamal <105470763+mahamjamal@users.noreply.github.com>
2024-03-19 23:19:58 -0600

    save and edit button in corner

--- uconnect/client/src/profile/Profile.js
+++ uconnect/client/src/profile/Profile.js
@@ -3,7 +3,7 @@ import "./Profile.css";
 import default_picture from "../images/default_picture.jpg";
 import { auth } from "../firebase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPen, faX } from "@fortawesome/free-solid-svg-icons";
+import { faPen, faX, faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 function Profile() {
   const [isEditing, setIsEditing] = useState(false);
@@ -56,15 +56,21 @@ function Profile() {
         <div className="profile-info">
           <div className="profile-header">
             {/* Avatar */}
-            {!isEditing ? (
-              <img
-                src={profileInfo.picture || default_picture}
-                alt="Profile"
-                className="avatar"
-              />
-            ) : (
-              <input type="file" onChange={handleImageUpload} />
-            )}
+            <div className="image-upload">
+              {!isEditing ? (
+                <img
+                  src={profileInfo.picture || default_picture}
+                  alt="Profile"
+                  className="avatar"
+                />
+              ) : (
+                <input
+                  type="file"
+                  onChange={handleImageUpload}
+                  className="image-input"
+                />
+              )}
+            </div>
 
             <div className="profile-name-email">
               {/* Name */}
@@ -72,6 +78,7 @@ function Profile() {
                 <div className="displayName">{profileInfo.name}</div>
               ) : (
                 <input
+                  className="displayName-input"
                   type="text"
                   value={profileInfo.name}
                   onChange={(e) =>
@@ -84,69 +91,80 @@ function Profile() {
               <div className="email">{profileInfo.email}</div>
             </div>
           </div>
-          {/* Bio */}
-          <p>Bio</p>
-          {!isEditing ? (
-            <div className="bio">
-              {profileInfo.bio || "Edit to update bio!"}{" "}
-            </div>
-          ) : (
-            <input
-              type="text"
-              value={profileInfo.bio}
-              onChange={(e) =>
-                setProfileInfo({ ...profileInfo, bio: e.target.value })
-              }
-            />
-          )}
+          <div className="profile-bio-interests">
+            {/* Bio */}
+            <p>Bio</p>
+            {!isEditing ? (
+              <div className="bio">
+                {profileInfo.bio || "Edit to update bio!"}{" "}
+              </div>
+            ) : (
+              <textarea
+                className="bio-input"
+                type="text"
+                value={profileInfo.bio}
+                onChange={(e) =>
+                  setProfileInfo({ ...profileInfo, bio: e.target.value })
+                }
+              />
+            )}
 
-          {/* Interests */}
-          <div className="interests-container">
-            <p>Interests</p>
-            <div className="interests">
-              {!isEditing ? (
-                profileInfo.interests.length > 0 ? (
-                  profileInfo.interests.map((interest) => (
-                    <span key={interest} className="interest-block">
-                      {interest}
-                    </span>
-                  ))
+            {/* Interests */}
+            <div className="interests-container">
+              <p>Interests</p>
+              <div className="interests">
+                {!isEditing ? (
+                  profileInfo.interests.length > 0 ? (
+                    profileInfo.interests.map((interest) => (
+                      <span key={interest} className="interest-block">
+                        {interest}
+                      </span>
+                    ))
+                  ) : (
+                    "Add an interest!"
+                  )
                 ) : (
-                  "Add an interest!"
-                )
-              ) : (
-                <>
-                  {profileInfo.interests.map((interest) => (
-                    <div key={interest} className="interest-block">
-                      {interest}
-                      <button className="profile-button" onClick={() => handleRemoveInterest(interest)}>
-                        <FontAwesomeIcon icon={faX} />
-                      </button>
-                    </div>
-                  ))}
-                  <input
-                    type="text"
-                    value={newInterest}
-                    onChange={(e) => setNewInterest(e.target.value)}
-                  />
-                  <button onClick={handleAddInterest}>Add Interest</button>
-                </>
-              )}
+                  <>
+                    {profileInfo.interests.map((interest) => (
+                      <div key={interest} className="interest-block">
+                        {interest}
+                        <button
+                          className="remove-button"
+                          onClick={() => handleRemoveInterest(interest)}
+                        >
+                          <FontAwesomeIcon icon={faX} />
+                        </button>
+                      </div>
+                    ))}
+                    <input
+                      type="text"
+                      value={newInterest}
+                      onChange={(e) => setNewInterest(e.target.value)}
+                    />
+                    <button
+                      classsName="add-interest"
+                      onClick={handleAddInterest}
+                    >
+                      Add Interest
+                    </button>
+                  </>
+                )}
+              </div>
             </div>
           </div>
         </div>
 
         {/* Edit Icon or Save Button based on isEditing state*/}
         {!isEditing ? (
-          <button onClick={handleEditClick} className="edit-icon">
+          <button className="edit-icon" onClick={handleEditClick}>
             <FontAwesomeIcon icon={faPen} />
           </button>
         ) : (
           <button
             onClick={() => handleSaveClick(profileInfo)}
-            className="save-button"
+            className="save-icon"
           >
-            Save
+            <FontAwesomeIcon icon={faBookmark} />
           </button>
         )}
       </div>
