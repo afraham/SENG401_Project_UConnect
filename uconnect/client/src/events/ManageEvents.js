313b1ed53c67bbc678e14a1e7a235f030ffd4bd5
Maham Jamal <105470763+mahamjamal@users.noreply.github.com>
2024-03-21 13:46:55 -0600

    getting started on request css

--- uconnect/client/src/events/ManageEvents.js
+++ uconnect/client/src/events/ManageEvents.js
@@ -1,68 +1,95 @@
 import React, { useState } from "react";
+import "./ManageEvents.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheck,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
 
-const ManageEvents = ({ closePopup, event, title, setCurrent, refetchEvents }) => {
-
-  const [handledRequests, setHandledRequests] = useState(event.pending.map(() => 'unhandled'))
+const ManageEvents = ({
+  closePopup,
+  event,
+  title,
+  setCurrent,
+  refetchEvents,
+}) => {
+  const [handledRequests, setHandledRequests] = useState(
+    event.pending.map(() => "unhandled")
+  );
 
   const handleClosePopup = () => {
     refetchEvents();
     closePopup();
-  }
+  };
   const handleApprove = async (userEmail, index) => {
-
     if (event.spotsTaken >= event.maxPeople) {
-      console.log("Max amount of users already in event, please add more spots if you would like to accept more users.") // Perhaps add a display to user to indicate this?
-    }
-    else {
+      console.log(
+        "Max amount of users already in event, please add more spots if you would like to accept more users."
+      ); // Perhaps add a display to user to indicate this?
+    } else {
       try {
-        const response = await fetch(`http://localhost:8000/api/events/approve/${event._id}`, {
-            method: 'PUT',
+        const response = await fetch(
+          `http://localhost:8000/api/events/approve/${event._id}`,
+          {
+            method: "PUT",
             headers: {
-                'Content-Type': 'application/json',
+              "Content-Type": "application/json",
             },
-            body: JSON.stringify({ userEmail }) // Just pass userEmail directly
-        });
-  
+            body: JSON.stringify({ userEmail }), // Just pass userEmail directly
+          }
+        );
+
         if (response.ok) {
-          setHandledRequests(prevReqs => {
+          setHandledRequests((prevReqs) => {
             const newReqs = [...prevReqs];
-            newReqs[index] = 'approved'; // Update the status of the item
+            newReqs[index] = "approved"; // Update the status of the item
             return newReqs;
           });
         } else {
-            console.error('Request to approve failed:', response.status, response.statusText);
-            // Handle error case here
+          console.error(
+            "Request to approve failed:",
+            response.status,
+            response.statusText
+          );
+          // Handle error case here
         }
       } catch (error) {
-          console.error('Error approving user:', error);
-          // Handle error case here
+        console.error("Error approving user:", error);
+        // Handle error case here
       }
     }
-  }
+  };
   const handleDeny = async (userEmail, index) => {
     try {
-      const response = await fetch(`http://localhost:8000/api/events/deny/${event._id}`, {
-          method: 'PUT',
+      const response = await fetch(
+        `http://localhost:8000/api/events/deny/${event._id}`,
+        {
+          method: "PUT",
           headers: {
-              'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify({ userEmail }) // Just pass userEmail directly
-      });
+          body: JSON.stringify({ userEmail }), // Just pass userEmail directly
+        }
+      );
       if (response.ok) {
-        setHandledRequests(prevReqs => {
+        setHandledRequests((prevReqs) => {
           const newReqs = [...prevReqs];
-          newReqs[index] = 'denied'; // Update the status of the item
+          newReqs[index] = "denied"; // Update the status of the item
           return newReqs;
         });
       } else {
-        console.error('Request to deny failed:', response.status, response.statusText);
+        console.error(
+          "Request to deny failed:",
+          response.status,
+          response.statusText
+        );
         // Handle error case here
       }
     } catch (error) {
-        console.error('Error denying user:', error);
-        // Handle error case here
+      console.error("Error denying user:", error);
+      // Handle error case here
     }
-  }
+  };
 
   return (
     <div className="popup-container">
@@ -72,21 +99,33 @@ const ManageEvents = ({ closePopup, event, title, setCurrent, refetchEvents }) =
           <div className="requests">
             <h3>Requests</h3>
             <div className="request-list">
-              {Array.isArray(event.pending) && event.pending.map((request, index) =>
-              <div className="request" key={index}>
-                <h1>{request}</h1>
-                {handledRequests[index] === 'unhandled' && (
-                  <div>
-                    <button onClick={() => handleApprove(request, index)}>Approve</button>
-                    <button onClick={() => handleDeny(request, index)}>Deny</button>
+              {Array.isArray(event.pending) &&
+                event.pending.map((request, index) => (
+                  <div className="request" key={index}>
+                    <h1>{request}</h1>
+                    {handledRequests[index] === "unhandled" && (
+                      <div>
+                        <button
+                          className="approve-button"
+                          onClick={() => handleApprove(request, index)}
+                        >
+                          <FontAwesomeIcon icon={faCheck} />
+                        </button>
+                        <button
+                          className="deny-button"
+                          onClick={() => handleDeny(request, index)}
+                        >
+                          <FontAwesomeIcon icon={faX} />{" "}
+                        </button>
+                      </div>
+                    )}
+                    {handledRequests[index] === "denied" && <span>Denied</span>}
+                    {handledRequests[index] === "approved" && (
+                      <span>Accepted</span>
+                    )}
                   </div>
-                )}
-                {handledRequests[index] === 'denied' && <span>Denied</span>}
-                {handledRequests[index] === 'approved' && <span>Accepted</span>}
-              </div>
-              )}
+                ))}
             </div>
-
           </div>
           <button className="close-button" onClick={handleClosePopup}>
             X
