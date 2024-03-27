d032b414c3d792e4bfec671bd54146b0e05532f7
Maham Jamal <105470763+mahamjamal@users.noreply.github.com>
2024-03-26 22:51:21 -0600

    css updated for add event pop up

--- uconnect/client/src/events/AddEvents.js
+++ uconnect/client/src/events/AddEvents.js
@@ -3,26 +3,28 @@ import "./AddEvents.css";
 import { auth } from "../firebase";
 
 const AddEvents = ({ closePopup, event, editMode, updateEvents }) => {
-	const [title, setTitle] = useState(event ? event.title : "");  // State for the event title, with a default value for a new event
-	const [description, setDescription] =  useState(event ? event.description : ""); // State for the event description
-	const [maxPeople, setMaxPeople] = useState(event ? event.maxPeople : 2);// State for  max people, should start with 2 min
-	const [date, setDate] = useState(event ? event.date : "");
-	const [location, setLocation] = useState(event ? event.location : "");
-	const [spotsTaken] = useState(event ? event.spotsTaken : 0);
-	const maxCharacters = 24; // Only 24 Characters allowed for title
+  const [title, setTitle] = useState(event ? event.title : ""); // State for the event title, with a default value for a new event
+  const [description, setDescription] = useState(
+    event ? event.description : ""
+  ); // State for the event description
+  const [maxPeople, setMaxPeople] = useState(event ? event.maxPeople : 2); // State for  max people, should start with 2 min
+  const [date, setDate] = useState(event ? event.date : "");
+  const [location, setLocation] = useState(event ? event.location : "");
+  const [spotsTaken] = useState(event ? event.spotsTaken : 0);
+  const maxCharacters = 24; // Only 24 Characters allowed for title
 
-	// Format the current date and time to not allow passed dates
-	const currentDateTime = new Date().toISOString().slice(0, 16);
+  // Format the current date and time to not allow passed dates
+  const currentDateTime = new Date().toISOString().slice(0, 16);
 
-	// Calculate maximum date (2 years from now)
-	const maxDate = new Date();
-    maxDate.setFullYear(maxDate.getFullYear() + 2);
-    const maxDateTime = maxDate.toISOString().slice(0, 16);
-	
-	// Function to increment maxPeople
-	const incrementPeople = () => {
-		setMaxPeople((prev) => prev + 1);
-	};
+  // Calculate maximum date (2 years from now)
+  const maxDate = new Date();
+  maxDate.setFullYear(maxDate.getFullYear() + 2);
+  const maxDateTime = maxDate.toISOString().slice(0, 16);
+
+  // Function to increment maxPeople
+  const incrementPeople = () => {
+    setMaxPeople((prev) => prev + 1);
+  };
 
   // Function to decrement maxPeople
   const decrementPeople = () => {
@@ -80,12 +82,12 @@ const AddEvents = ({ closePopup, event, editMode, updateEvents }) => {
       location,
     });
 
-		closePopup();
+    closePopup();
 
-		// send the event data to the server
-		try {
-			const user = auth.currentUser; // get the current user
-			const userEmail = user ? user.email : null; // get the user's email
+    // send the event data to the server
+    try {
+      const user = auth.currentUser; // get the current user
+      const userEmail = user ? user.email : null; // get the user's email
 
       const pending = [];
       const approved = [];
@@ -111,20 +113,20 @@ const AddEvents = ({ closePopup, event, editMode, updateEvents }) => {
         }),
       });
 
-			// Checking the server's response
-			if (response.ok) {
-				console.log("Event data sent successfully");
-				updateEvents();
-				closePopup();
-			} else {
-				console.error("Failed to send event data");
-			}
-		} catch (error) {
-			console.error("Error sending event data:", error);
-		}
-	};
+      // Checking the server's response
+      if (response.ok) {
+        console.log("Event data sent successfully");
+        updateEvents();
+        closePopup();
+      } else {
+        console.error("Failed to send event data");
+      }
+    } catch (error) {
+      console.error("Error sending event data:", error);
+    }
+  };
 
-	/*
+  /*
     handleUpdateEvent
     Updates existing event data in the database. Performs field checks similar to saveEventData,
     but sends a PATCH request to a specific event endpoint.
@@ -134,104 +136,116 @@ const AddEvents = ({ closePopup, event, editMode, updateEvents }) => {
     
     Returns: None but updates the event list in the parent component and closes the popup on success.
     */
-	const handleUpdateEvent = async (eventId) => {
-		try {
-			if (
-				!title.trim() ||
-				!description.trim() ||
-				!date.trim() ||
-				!location.trim()
-			) {
-				alert("Please fill in all fields.");
-				return; // Stop the function if any field is empty
-			}
-	
-			const user = auth.currentUser; // get the current user
-			const userEmail = user ? user.email : null; // get the user's email
-	
-			const updatedEventData = {
-				title,
-				description,
-				maxPeople,
-				date,
-				location,
-				userEmail,
-			};
-	
-			// Sending the updated event data to the server using a PATCH request for partial update
-			const response = await fetch(`http://localhost:8000/api/events/${eventId}/edit`, {
-				method: "PATCH", // Use PATCH method for partial updates
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(updatedEventData),
-			});
-	
-			if (response.ok) {
-				console.log("Event data updated successfully");
-				updateEvents();
-				closePopup();
-			} else {
-				console.error("Failed to update event data desc:", response.status);
-				console.error("Response data:", await response.json());
-			}
-		} catch (error) {
-			console.error("Error updating event data:", error);
-		}
-	};
-	
-	// Rendering the Popup UI after clicking Add an Event.
-	return (
-		<div className="popup-container">
-			<div className="popup-content">
-				<div className="ce-header">
-					<h2 className="ce-header">{editMode ? "Edit Event" : "Add New Event"}</h2>
-					<button className="close-button" onClick={closePopup}>
-						X
-					</button>
-				</div>
-				<input
-					type="text"
-					placeholder="Title"
-					value={title}
-					onChange={(e) => handleInputChange(e.target.value, setTitle)}
-				/>{" "}
-				{/*Title Box*/}
-				<textarea
-					placeholder="Description"
-					value={description}
-					onChange={(e) => setDescription(e.target.value)}
-				/>{" "}
-				{/* Descr Box*/}
-				<div className="max-people">
-					{" "}
-					{/*No. of People*/}
-					<p>Max People: </p>
-					<button onClick={decrementPeople}>-</button>
-					<span>{maxPeople}</span>
-					<button onClick={incrementPeople}>+</button>
-				</div>
-				<input
-					type="datetime-local"
-					value={date}
-					onChange={(e) => setDate(e.target.value)}
-					min={currentDateTime} 
-					max={maxDateTime}
-				/>
-				<input
-					type="text"
-					placeholder="Location"
-					value={location}
-					onChange={(e) => setLocation(e.target.value)}
-				/>
-				<div className="create-button-container">
-					<button className="create-button" onClick={editMode ? () => handleUpdateEvent(event._id) : saveEventData}>
-						{editMode ? "Save Changes" : "Create"}
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  const handleUpdateEvent = async (eventId) => {
+    try {
+      if (
+        !title.trim() ||
+        !description.trim() ||
+        !date.trim() ||
+        !location.trim()
+      ) {
+        alert("Please fill in all fields.");
+        return; // Stop the function if any field is empty
+      }
+
+      const user = auth.currentUser; // get the current user
+      const userEmail = user ? user.email : null; // get the user's email
+
+      const updatedEventData = {
+        title,
+        description,
+        maxPeople,
+        date,
+        location,
+        userEmail,
+      };
+
+      // Sending the updated event data to the server using a PATCH request for partial update
+      const response = await fetch(
+        `http://localhost:8000/api/events/${eventId}/edit`,
+        {
+          method: "PATCH", // Use PATCH method for partial updates
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedEventData),
+        }
+      );
+
+      if (response.ok) {
+        console.log("Event data updated successfully");
+        updateEvents();
+        closePopup();
+      } else {
+        console.error("Failed to update event data desc:", response.status);
+        console.error("Response data:", await response.json());
+      }
+    } catch (error) {
+      console.error("Error updating event data:", error);
+    }
+  };
+
+  // Rendering the Popup UI after clicking Add an Event.
+  return (
+    <div className="popup-container">
+      <div className="popup-content">
+        <div className="add-event-form">
+          <div className="ce-header">
+            <h2 className="ce-header">
+              {editMode ? "Edit Event" : "Add New Event"}
+            </h2>
+            <button className="close-button" onClick={closePopup}>
+              X
+            </button>
+          </div>
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => handleInputChange(e.target.value, setTitle)}
+          />{" "}
+          {/*Title Box*/}
+          <textarea
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />{" "}
+          {/* Descr Box*/}
+          <div className="max-people">
+            {" "}
+            {/*No. of People*/}
+            <p>Max People: </p>
+            <button onClick={decrementPeople}>-</button>
+            <span>{maxPeople}</span>
+            <button onClick={incrementPeople}>+</button>
+          </div>
+          <input
+            type="datetime-local"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            min={currentDateTime}
+            max={maxDateTime}
+          />
+          <input
+            type="text"
+            placeholder="Location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <div className="create-button-container">
+            <button
+              className="create-button"
+              onClick={
+                editMode ? () => handleUpdateEvent(event._id) : saveEventData
+              }
+            >
+              {editMode ? "Save Changes" : "Create"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddEvents;
